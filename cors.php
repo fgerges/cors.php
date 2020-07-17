1f84032c137db73a2cbdeac66f356d29c0c00000
Fabrice Gergès <68105056+fgerges@users.noreply.github.com>
2020-07-17 16:42:37 +0200

    Add files via upload

---
+++ cors.php
@@ -0,0 +1,19 @@
+<?php
+
+// CORS
+// depuis n'importe où
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache 1 jour
+}
+// Access-Control reçu lors de la requête OPTIONS
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+    }
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    }
+    exit(0);
+}
